3eaeac76526972c2cdcb145fe90afee0d1750d40
Harry Bosch <harrybosch03@gmail.com>
2023-07-31 09:24:07 +1000

    Backup

--- Assets/Code/Editor/Inspector/TrackModel.cs
+++ Assets/Code/Editor/Inspector/TrackModel.cs
@@ -7,13 +7,31 @@ namespace FR8Editor.Inspector
     [CustomEditor(typeof(TrackModel))]
     public class TrackModelEditor : Editor
     {
+        private bool autoBakeMesh = false;
+
+        [MenuItem("Actions/Track Model/Bake All Meshes")]
+        public static void BakeAllTracks()
+        {
+            var list = FindObjectsOfType<TrackModel>();
+            foreach (var e in list)
+            {
+                e.BakeMesh();
+            }
+        }
+        
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
 
-            var target = base.target as TrackModel;
+            var target = this.target as TrackModel;
             if (!target) return;
 
+            // ReSharper disable once AssignmentInConditionalExpression
+            if (autoBakeMesh = EditorGUILayout.Toggle("Auto Bake Mesh", autoBakeMesh))
+            {
+                target.BakeMesh();
+            }
+            
             if (GUILayout.Button("Bake", GUILayout.Height(30)))
             {
                 target.BakeMesh();
