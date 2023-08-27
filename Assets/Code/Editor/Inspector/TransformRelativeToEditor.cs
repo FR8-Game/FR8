627d8d58dcc75323b7f311d45e492e3edfcfeea0
Harry Bosch/Laptop <harrybosch03@gmail.com>
2023-08-27 18:36:34 +1000

    Added Transform Relative To Tool

---
+++ Assets/Code/Editor/Inspector/TransformRelativeToEditor.cs
@@ -0,0 +1,29 @@
+﻿using FR8Runtime.Tools;
+using UnityEditor;
+using UnityEngine;
+
+namespace FR8Editor.Inspector
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TransformRelativeTo))]
+    public class TransformRelativeToEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            var t = target as TransformRelativeTo;
+            if (!t) return;
+            
+            EditorGUILayout.Space();
+
+            
+            var icon = EditorGUIUtility.IconContent(t.Locked ? "LockIcon-On" : "LockIcon").image;
+            var content = new GUIContent(t.Locked ? "Locked" : "Unlocked", icon);
+            if (GUILayout.Button(content, GUILayout.Height(40)))
+            {
+                t.Locked = !t.Locked;
+            }
+        }
+    }
+}
