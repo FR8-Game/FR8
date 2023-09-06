81a5dbc371f3727f31fc968f42be2645b02b47bf
Harry Bosch <harrybosch03@gmail.com>
2023-09-06 16:16:58 +1000

    Backup

--- Assets/Code/Scripts/Player/Submodules/PlayerUI.cs
+++ Assets/Code/Scripts/Player/Submodules/PlayerUI.cs
@@ -1,110 +1,64 @@
 ﻿using System;
 using FR8Runtime.CodeUtility;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Compass = FR8Runtime.UI.CustomControls.Compass;
 
 namespace FR8Runtime.Player.Submodules
 {
     [Serializable]
     public class PlayerUI
     {
-        [SerializeField] private Color healthColor = Color.red;
-        [SerializeField] private Color shieldColor = Color.cyan;
-
-        [Space]
-        [SerializeField] private float fadeTime = 0.4f;
-
-        [SerializeField] private AnimationCurve vignetteFadeCurve = AnimationCurve.Linear(0.0f, 1.0f, 1.0f, 0.0f);
-        [SerializeField] private AnimationCurve healthVignetting = AnimationCurve.Linear(0.0f, 1.0f, 1.0f, 0.0f);
-
-        [Space]
-        [Range(0.0f, 1.0f)] [SerializeField] private float vignetteMaxAlpha = 0.3f;
-
         private PlayerAvatar avatar;
-        
-        private GameObject deathMenu;
-        private Image vignette;
-        private Image healthBar;
-        private Image shieldBar;
+        private UIDocument hud;
+
+        private ProgressBar shieldsFill;
+        private Label shieldsText;
+        private Compass compass;
 
         private const string VitalityRootPath = "UI/Vitality";
         private const string CoverPath = VitalityRootPath + "/Death";
-        private const string VignettePath = VitalityRootPath + "/Vignette";
-        private const string HealthBarPath = VitalityRootPath + "/Stats/Health";
-        private const string ShieldBarPath = VitalityRootPath + "/Stats/Shields";
         
         public void Init(PlayerAvatar avatar)
         {
             this.avatar = avatar;
 
-            avatar.UpdateEvent += Update;
+            avatar.vitality.IsAliveChangedEvent += UpdateDeathUI;
             avatar.vitality.HealthChangeEvent += UpdateBars;
-            avatar.vitality.IsAliveChangedEvent += ShowDeathUI;
+            avatar.UpdateEvent += Update;
             
-            deathMenu = FindUtility.Find(avatar.transform, CoverPath).gameObject;
-            vignette = FindUtility.Find<Image>(avatar.transform, VignettePath);
-            healthBar = FindUtility.Find<Image>(avatar.transform, $"{HealthBarPath}/Fill");
-            shieldBar = FindUtility.Find<Image>(avatar.transform, $"{ShieldBarPath}/Fill");
+            hud = avatar.transform.Find("HUD").GetComponent<UIDocument>();
+            var root = hud.rootVisualElement;
+
+            shieldsFill = root.Q<ProgressBar>("shields-bar");
+            shieldsText = root.Q<Label>("shields-text");
+            compass = root.Q<Compass>("compass");
             
             SetupDeathUI();
-
-            OnValidate(avatar);
-            UpdateBars();
-        }
-
-        private void SetupDeathUI()
-        {
-            var button = FindUtility.Find<Button>(deathMenu.transform, "Button");
-            UIUtility.MakeButtonList(button,
-                ("Reconstitute", avatar.vitality.Revive)
-            );
-            
-            ShowDeathUI();
-        }
-
-        private void UpdateBars()
-        {
-            var v = avatar.vitality;
-
-            healthBar.fillAmount = Mathf.Clamp01((float)v.CurrentHealth / v.maxHealth);
-            shieldBar.fillAmount = Mathf.Clamp01((float)v.CurrentShields / v.shieldDuration);
-        }
-
-        private void ShowDeathUI()
-        {
-            deathMenu.SetActive(!avatar.IsAlive);
-        }
-
-        public void OnValidate(PlayerAvatar avatar)
-        {
-            const float bgDarkness = 0.2f;
-
-            var h = FindUtility.Find<Image>(avatar.transform, HealthBarPath);
-            var hf = FindUtility.Find<Image>(avatar.transform, $"{HealthBarPath}/Fill");
-
-            if (h) h.color = new Color(healthColor.r * bgDarkness, healthColor.g * bgDarkness, healthColor.b * bgDarkness, healthColor.a);
-            if (hf) hf.color = healthColor;
-
-            var s = FindUtility.Find<Image>(avatar.transform, ShieldBarPath);
-            var sf = FindUtility.Find<Image>(avatar.transform, $"{ShieldBarPath}/Fill");
-
-            if (s) s.color = new Color(shieldColor.r * bgDarkness, shieldColor.g * bgDarkness, shieldColor.b * bgDarkness, shieldColor.a);
-            if (sf) sf.color = shieldColor;
         }
 
         private void Update()
         {
-            var t = Mathf.Clamp01((Time.time - avatar.vitality.LastDamageTime) / fadeTime);
-            var alpha = vignetteFadeCurve.Evaluate(t);
+            compass.FaceAngle = avatar.transform.eulerAngles.y;
+        }
+        
+        private void UpdateBars()
+        {
+            shieldsFill.value = avatar.vitality.CurrentShields / avatar.vitality.shieldDuration;
+            shieldsText.text = $"<size=50%>SHIELDS</size>\n[ {(avatar.vitality.Exposed ? "ACTIVE" : " IDLE ")} | {Mathf.FloorToInt(avatar.vitality.CurrentShields),2:N0} s ]";
+        }
 
-            var normalizedHealth = avatar.vitality.CurrentHealth / (float)avatar.vitality.maxHealth;
-            alpha = Mathf.Max(alpha, healthVignetting.Evaluate(normalizedHealth));
+        private void SetupDeathUI()
+        {
             
-            if (vignette)
-            {
-                vignette.color = new Color(healthColor.r, healthColor.g, healthColor.b, alpha * vignetteMaxAlpha);
-            }
+            
+            UpdateDeathUI();
+        }
+
+        private void UpdateDeathUI()
+        {
+            
         }
     }
 }
