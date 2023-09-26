d89ca09c201a5782b349b5606a03c1bec212adbc
Harry Bosch <harrybosch03@gmail.com>
2023-09-26 17:06:01 +1000

    EOD Backup

---
+++ Assets/Code/Scripts/Player/PlayerMount.cs
@@ -0,0 +1,61 @@
+using FR8Runtime.Interactions.Drivers.Submodules;
+using UnityEngine;
+
+namespace FR8Runtime.Player
+{
+    [SelectionBase]
+    [DisallowMultipleComponent]
+    public class PlayerMount : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private bool limitRotation;
+        [SerializeField] private Vector2 rotationLimit;
+        [SerializeField] private Vector2 rotationLimitOffset;
+        [SerializeField] private Vector3 dismountOffset;
+
+        private Rigidbody body;
+        
+        public bool CanInteract => true;
+        public string DisplayName => name;
+        public string DisplayValue => "";
+        public bool OverrideInteractDistance => false;
+        public float InteractDistance { get; }
+        
+        public Vector3 Position => transform.TransformPoint(offset);
+        public Quaternion Rotation => transform.rotation;
+        public Vector3 Velocity => body ? body.velocity : Vector3.zero;
+        public Vector3 DismountPosition => transform.TransformPoint(dismountOffset);
+
+        public Quaternion ConstrainRotation(Quaternion q)
+        {
+            if (!limitRotation) return q.normalized;
+            
+            var rotation = new Vector2(q.eulerAngles.y, q.eulerAngles.x);
+
+            rotation -= rotationLimitOffset;
+
+            rotation.x = Mathf.Clamp(rotation.x, -rotationLimit.x, rotationLimit.x);
+            rotation.y = Mathf.Clamp(rotation.y, -rotationLimit.y, rotationLimit.y);
+            
+            rotation += rotationLimitOffset;
+            return Quaternion.Euler(rotation.y, rotation.x, 0.0f);
+        }
+        
+        private void Awake()
+        {
+            body = GetComponentInParent<Rigidbody>();
+        }
+
+        public void Nudge(int direction) { }
+
+        public void BeginInteract(GameObject interactingObject)
+        {
+            if (interactingObject.TryGetComponent(out PlayerAvatar avatar))
+            {
+                avatar.SetMount(this);
+            }
+        }
+
+        public void ContinueInteract(GameObject interactingObject) { }
+    }
+}
