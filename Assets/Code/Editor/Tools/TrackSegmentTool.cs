c4cacfc6489f76223610f55671dc155295827b9a
MarshallTheDev <93643324+MarshallTheDev@users.noreply.github.com>
2023-09-27 15:34:34 +1000

    Tutorial Contract & Bosch Fixed Track Segment Issue

--- Assets/Code/Editor/Tools/TrackSegmentTool.cs
+++ Assets/Code/Editor/Tools/TrackSegmentTool.cs
@@ -26,6 +26,12 @@ namespace FR8Editor.Tools
 
         public static float handleScale = 1.0f;
 
+        public override void OnActivated()
+        {
+            all = new List<TrackSegment>(FindObjectsOfType<TrackSegment>());
+            terrainList = new List<Terrain>(FindObjectsOfType<Terrain>());
+        }
+
         public Transform[] Knots()
         {
             if (!trackSegment) return new Transform[0];
@@ -42,9 +48,6 @@ namespace FR8Editor.Tools
 
         public override void OnToolGUI(EditorWindow window)
         {
-            if (all == null) all = new List<TrackSegment>(FindObjectsOfType<TrackSegment>());
-            if (terrainList == null) terrainList = new List<Terrain>(FindObjectsOfType<Terrain>());
-
             var gameObject = target as GameObject;
             if (!gameObject) return;
 
