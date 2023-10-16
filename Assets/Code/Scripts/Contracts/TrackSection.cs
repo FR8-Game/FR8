6a74f270288f1ae2756fadc88ae36a925f904985
Harry Bosch <harrybosch03@gmail.com>
2023-10-16 12:23:16 +1100

    Added Zone to Track Segment

--- Assets/Code/Scripts/Contracts/TrackSection.cs
+++ Assets/Code/Scripts/Contracts/TrackSection.cs
@@ -8,6 +8,9 @@ namespace FR8Runtime.Contracts
     {
         [SerializeField] private TrackSegment track;
         [SerializeField] private GameObject signPrefab;
+        [SerializeField] private GameObject zone;
+        [SerializeField] private float zoneWidth;
+        [SerializeField] private float zoneHeight;
         [SerializeField] public Vector3 localStartPosition;
         [SerializeField] public Quaternion localStartRotation = Quaternion.identity;
         [SerializeField] public Vector3 localEndPosition;
@@ -18,45 +21,78 @@ namespace FR8Runtime.Contracts
 
         public Vector3 StartPosition
         {
-            get => transform.TransformPoint(localStartPosition); 
+            get => transform.TransformPoint(localStartPosition);
             set => localStartPosition = transform.InverseTransformPoint(value);
         }
+
         public Quaternion StartRotation
         {
             get => transform.rotation * localStartRotation;
             set => localStartRotation = Quaternion.Inverse(transform.rotation) * value;
         }
+
         public Vector3 EndPosition
         {
-            get => transform.TransformPoint(localEndPosition); 
+            get => transform.TransformPoint(localEndPosition);
             set => localEndPosition = transform.InverseTransformPoint(value);
         }
+
         public Quaternion EndRotation
         {
             get => transform.rotation * localEndRotation;
             set => localEndRotation = Quaternion.Inverse(transform.rotation) * value;
         }
-        
+
         public float SectionStart { get; private set; }
         public float SectionEnd { get; private set; }
         public string Name => name;
         public TrackSegment Track => track;
-        
+
         private void Start()
         {
             OnValidate();
-            
+
             instanceSign(SectionStart, -1.0f);
             instanceSign(SectionEnd, 1.0f);
 
             void instanceSign(float t, float facing)
             {
                 if (!signPrefab) return;
-                
+
                 var instance = Instantiate(signPrefab, transform).transform;
                 instance.position = track.SamplePoint(t);
                 instance.rotation = Quaternion.LookRotation(track.SampleTangent(t) * facing, Vector3.up);
             }
+
+            SetupZone();
+        }
+
+        private void SetupZone()
+        {
+            var find = transform.Find("Zone");
+            var zone = find ? find.gameObject : null;
+            if (!zone && this.zone)
+            {
+#if UNITY_EDITOR
+                zone = (GameObject)UnityEditor.PrefabUtility.InstantiatePrefab(this.zone);
+#else
+                zone = Instantiate(this.zone);
+#endif
+                zone.transform.SetParent(transform);
+                zone.name = "Zone";
+            }
+
+            if (!zone) return;
+
+            var start = StartPosition;
+            var end = EndPosition;
+            var center = (start + end) / 2.0f;
+            var vector = end - start;
+            var length = vector.magnitude;
+
+            zone.transform.rotation = Quaternion.LookRotation(vector, Vector3.up);
+            zone.transform.position = center + zone.transform.up * (zoneHeight / 2.0f - 1.0f);
+            zone.transform.localScale = new Vector3(zoneWidth, zoneHeight, length);
         }
 
         public bool Contains(Vector3 point)
@@ -64,12 +100,14 @@ namespace FR8Runtime.Contracts
             var t = track.GetClosestPoint(point, false);
             return t > SectionStart && t < SectionEnd;
         }
-        
+
         private void OnValidate()
         {
             if (!track) return;
 
             UpdateEnds();
+
+            SetupZone();
         }
 
         private void OnDrawGizmos()
@@ -85,7 +123,7 @@ namespace FR8Runtime.Contracts
             {
                 Gizmos.DrawLine(track.SamplePoint(Mathf.Lerp(SectionStart, SectionEnd, p)), track.SamplePoint(Mathf.Lerp(SectionStart, SectionEnd, p + step)));
             }
-            
+
             HBCore.Utility.GizmoUtility.Label((StartPosition + EndPosition) / 2.0f + Vector3.up * 10.0f, name);
 
             void drawEndCap(float t, Vector3 from, float scale = 1.0f)
@@ -95,7 +133,7 @@ namespace FR8Runtime.Contracts
 
                 Gizmos.matrix = Matrix4x4.identity;
                 Gizmos.DrawLine(position, from);
-                
+
                 var matrix = Matrix4x4.TRS(position, Quaternion.LookRotation(tangent, Vector3.up), Vector3.one);
                 Gizmos.matrix = matrix;
 
@@ -113,7 +151,7 @@ namespace FR8Runtime.Contracts
 
             transform.position = (startPosition + endPosition) / 2.0f;
             transform.rotation = Quaternion.Slerp(startRotation, endRotation, 0.5f);
-            
+
             StartPosition = startPosition;
             StartRotation = startRotation;
             EndPosition = endPosition;
@@ -129,7 +167,7 @@ namespace FR8Runtime.Contracts
 
             var vec = point - position;
             var offset = vec - tangent * Vector3.Dot(vec, tangent);
-            
+
             position += offset;
             var rotation = Quaternion.LookRotation(track.SampleTangent(t), Vector3.up);
 
@@ -146,16 +184,16 @@ namespace FR8Runtime.Contracts
             if (StartPosition == lastStartPosition && EndPosition == lastEndPosition) return;
 
             UpdateEnds();
-            
+
             lastStartPosition = StartPosition;
             lastEndPosition = EndPosition;
         }
-        
+
         public void UpdateEnds()
         {
             GetTrackIfMissing();
             if (!track) return;
-            
+
             SectionStart = track.GetClosestPoint(StartPosition, true);
             SectionEnd = track.GetClosestPoint(EndPosition, true);
         }
@@ -164,7 +202,7 @@ namespace FR8Runtime.Contracts
         {
             localStartPosition = Vector3.back * 5.0f;
             localStartRotation = Quaternion.identity;
-            
+
             localEndPosition = Vector3.forward * 5.0f;
             localEndRotation = Quaternion.identity;
         }
