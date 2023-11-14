71e2767ac7a3eacfd8fe9a4e124412171b03ead3
HenryB19 <102206788+HenryB19@users.noreply.github.com>
2023-11-14 15:27:09 +1100

    Player sit sfx

--- Assets/Code/Scripts/References/SoundReference.cs
+++ Assets/Code/Scripts/References/SoundReference.cs
@@ -27,6 +27,7 @@ namespace FR8.Runtime.References
         public static readonly SoundReference Wind = "";
         public static readonly SoundReference WindowWipers = "";
         public static readonly SoundReference FuzeDoor = "{9a6df018-805a-4ee8-ab6d-7efd96814aee}";
+        public static readonly SoundReference PlayerSit = "{5b14c251-b25e-442c-8366-78a2ab6dcef2}";
 
         private string guid;
 
